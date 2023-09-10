b9639d8fd96678135d53dd0aad2a022cdd5b1b73
vikasboss6855 <vikassharma.0407@gmail.com>
2023-09-10 17:18:00 +0530

    change authors

---
+++ author.sh
@@ -0,0 +1,14 @@
+git filter-branch --env-filter '
+OLD_EMAIL="vikas.sharma@exotel.in"
+CORRECT_NAME="vikasboss6855"
+CORRECT_EMAIL="vikassharma.0407@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]; then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]; then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
